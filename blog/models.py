eabe66e5b3f2eb72103f3b9a1dfccaab292ef721
malinich <miltrix@yandex.ru>
2017-12-13 19:01:14 +0300

    add schema for additional information about a cities, county, geo tags of the blog

--- blog/models.py
+++ blog/models.py
@@ -9,8 +9,13 @@ from app import MetaBaseTemplate
 class Comment(EmbeddedDocument, metaclass=MetaBaseTemplate):
     user = fields.UUIDField(required=True)
     text = fields.StringField(validate=validate.Length(min=3), required=True)
-
     created = fields.DateTimeField(missing=datetime.datetime.now)
+
+
+class Areal(EmbeddedDocument, metaclass=MetaBaseTemplate):
+    country = fields.StringField(required=True)
+    cities = fields.ListField(fields.StringField, missing=list)
+    geo = fields.ListField(fields.DictField)
 
 
 class Blog(Document, metaclass=MetaBaseTemplate):
@@ -19,9 +24,9 @@ class Blog(Document, metaclass=MetaBaseTemplate):
     title = fields.StringField(validate=validate.Length(max=128), required=True)
     text = fields.StringField(validate=validate.Length(min=128), required=True)
     user = fields.UUIDField(required=True)
-    geo = fields.ListField(fields.DictField)
     created = fields.DateTimeField(missing=datetime.datetime.now)
     comments = fields.ListField(fields.EmbeddedField(Comment), missing=list)
+    areal = fields.EmbeddedField(Areal, missing=dict)
 
     class Meta:
         pass
